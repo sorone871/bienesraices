d89abc6fbe3b6d2ba706ca451a3c95026591fe99
Sorone <joseluis_rgoytia@hotmail.com>
2024-02-28 10:56:59 -0600

    preferencia de dark mode

--- src/js/app.js
+++ src/js/app.js
@@ -6,6 +6,22 @@ document.addEventListener('DOMContentLoaded', function(){
 });
 
 function darkMode(){
+
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+    // console.log(prefiereDarkMode.matches);
+    if (prefiereDarkMode.matches) {
+        document.body.classList.add('dark-mode');
+    }else{
+        document.body.classList.remove('dark-mode');
+    }
+
+    prefiereDarkMode.addEventListener('change', function(){
+        if (prefiereDarkMode.matches) {
+            document.body.classList.add('dark-mode');
+        }else{
+            document.body.classList.remove('dark-mode');
+        }
+    });
     const darkMode = document.querySelector('.dark-mode-boton');
 
     darkMode.addEventListener('click', function(){
